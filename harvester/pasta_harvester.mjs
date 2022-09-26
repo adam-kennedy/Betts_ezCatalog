a3b8997916cf650e8cae0cde427c4804218f734e
GitHub Action <action@github.com>
2022-09-26 23:39:28 +0000

    Configure ezCatalog

--- harvester/pasta_harvester.mjs
+++ harvester/pasta_harvester.mjs
@@ -8,7 +8,7 @@ import parser from 'xml2js';
 
 const PASTA_CONFIG = {
    "server": "https://pasta.lternet.edu/package/search/eml?", // PASTA server
-   "filter": '&fq=author:"Betts"', // Filter results on a unique keyword of a research group
+   "filter": '&q=id:edi.23+id:edi.101+id:edi.845', // Filter results on a unique keyword of a research group
    "limit": 2000, // Max number of results to retrieve per page
    "fields": [
       "taxonomic",
